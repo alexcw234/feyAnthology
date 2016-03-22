fbe77d4c9a5f84f0490746de1c3d65f8a084d5ad
Alex Watanabe <alexcw@hawaii.edu>
2016-03-21 15:23:40 -1000

    Add Database Service Provider

--- config/app.php
+++ config/app.php
@@ -155,7 +155,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
+        Bosnadev\Database\DatabaseServiceProvider::class,
     ],
 
     /*
