f3cc907d67bf0bb771fcf553d1bce636d8a9d6c0
Alex Watanabe <alexcw@hawaii.edu>
2016-03-22 13:37:16 -1000

    Draft model for Category

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,44 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+
+  /*
+  *     Model properties and attributes
+  */
+
+  use SoftDeletes;
+
   protected $table = 'categories';
   protected $primaryKey = 'catID';
-    //
+
+  protected $fillable = array('catName');
+  protected $guarded = array('catID');
+  protected $dates = ['deleted_at'];
+
+
+
+  /*
+  *     Relationship: Has many works
+  */
+  public function work() {
+      return $this->hasMany('Work','catID','catID');
+  }
+
+
+  /*
+  *     Relationship: Belongs to many users
+  */
+  public function user() {
+      return $this->belongsToMany('User','userID','userID');
+  }
+
+
+  /*
+  *     Relationship: Belongs to many groups
+  */
+  public function group() {
+      return $this->belongsToMany('Group','groupID','groupID');
+  }
+
+
 }
