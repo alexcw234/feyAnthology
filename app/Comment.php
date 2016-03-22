a5f178702b8610e42284bb95a620e8b73ee47b3a
Alex Watanabe <alexcw@hawaii.edu>
2016-03-22 13:48:43 -1000

    Draft model for Comment

--- app/Comment.php
+++ app/Comment.php
@@ -6,7 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+  /*
+  *     Model properties and attributes
+  */
+  use SoftDeletes;
+
   protected $table = 'comments';
   protected $primaryKey = 'commentID';
-    //
+
+  protected $fillable = array('workID','userID','comment');
+  protected $guarded = array('commentID');
+  protected $dates = ['deleted_at'];
+
+
+  /*
+  *     Relationship: Belongs to one work
+  */
+  public function work() {
+
+      return  $this->belongsTo('Work','workID','workID');
+
+  }
+
+
+  /*
+  *     Relationship: Belongs to one user
+  */
+  public function user() {
+
+      return  $this->belongsTo('User','userID','userID');
+
+  }
+
+
+
 }
