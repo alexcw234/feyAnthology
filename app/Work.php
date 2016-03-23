fce41956bf1e8d8edd7bad8b97bb2ed3b0595601
Alex Watanabe <alexcw@hawaii.edu>
2016-03-22 14:17:37 -1000

    Draft model for Work

--- app/Work.php
+++ app/Work.php
@@ -6,7 +6,55 @@ use Illuminate\Database\Eloquent\Model;
 
 class Work extends Model
 {
+
+  /*
+  *     Model properties and attributes
+  */
+  use SoftDeletes;
+
   protected $table = 'works';
   protected $primaryKey = 'workID';
-    //
+
+  protected $fillable = array('catID','infoID','url','approved','subID','appID');
+
+  protected $guarded = array('workID');
+
+  protected $dates = ['deleted_at'];
+
+  /*
+  *     Relationship: Has one Info
+  */
+  public function info() {
+      return $this->hasOne('Info','infoID','infoID');
+  }
+
+  /*
+  *     Relationship: Has many Comments
+  */
+  public function comment() {
+      return $this->hasMany('Comment','workID','workID');
+  }
+
+  /*
+  *     Relationship: Belongs to one category
+  */
+  public function category() {
+      return $this->belongsTo('Category','catID','catID');
+  }
+
+  /*
+  *     Relationship: SubID Belongs to one user
+  */
+  public function subID() {
+      return $this->belongsTo('User','userID','subID');
+  }
+
+  /*
+  *     Relationship: AppID Belongs to one user
+  */
+  public function appID() {
+      return $this->belongsTo('User','userID','appID');
+  }
+
+
 }
