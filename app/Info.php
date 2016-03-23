7d29ff7486c6235e2e0b414189bb889666de6c92
Alex Watanabe <alexcw@hawaii.edu>
2016-03-22 14:00:16 -1000

    Draft model for Info

--- app/Info.php
+++ app/Info.php
@@ -6,7 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Info extends Model
 {
+
+  /*
+  *     Model properties and attributes
+  */
+  use SoftDeletes;
+
   protected $table = 'info';
   protected $primaryKey = 'infoID';
-    //
+
+  protected $fillable = array('info');
+  protected $guarded = array('infoID');
+
+  protected $dates = ['deleted_at'];
+
+
+    /*
+    *     Relationship: Belongs to one work
+    */
+    public function work() {
+
+        return  $this->belongsTo('Work','workID','workID');
+
+    }
+
+
+
 }
