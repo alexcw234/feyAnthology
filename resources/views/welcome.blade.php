9f160d2ea09d560b7da09348ed8d386a2d83f4ae
Alex Watanabe <alexcw@hawaii.edu>
2016-03-23 15:42:05 -1000

    Modify welcome.blade to include an angular file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+
+<!--Angular scripts-->
+  <script src="bower_compontents/angular/angular.min.js"></script>
+  <script src="bower_compontents/angular/angular-ui-router/release/angular-ui-router.min.js"></script>
+<!--End angular scripts-->
+@include('welcome.angular');
+
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
