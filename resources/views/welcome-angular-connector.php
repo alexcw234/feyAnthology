818530d8d5e41c7ef3d68d3157a5763fcb011d5d
Alex Watanabe <alexcw@hawaii.edu>
2016-03-23 18:13:20 -1000

    Switch html echo to an actual file

--- resources/views/welcome-angular-connector.php
+++ resources/views/welcome-angular-connector.php
@@ -1,11 +1,4 @@
 <?php
-
-echo "
-<script src=\"js/welcome.js\"></script>
-
-                    <div ng-app=\"myApp\" ng-controller=\"myCtrl\">
-                    {{ firstName + \" \" + lastName }}
-                    </div>
-"
-
+set_include_path("../resources/templates");
+include("frontpage.html");
  ?>
