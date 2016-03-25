0a8d4271adaab9644c2f4e7bc19141abc3d54de4
Alex Watanabe <alexcw@hawaii.edu>
2016-03-24 15:35:17 -1000

    Begin work on cats controller

--- app/Http/Controllers/sample.php
+++ app/Http/Controllers/sample.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-class CatsController extends Controller
+class SampleController extends Controller
 {
     //
 
--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", []);
+var app = angular.module("browseApp", []);
 
 app.controller("myCtrl", function($scope) {
     $scope.firstName = "If this is showing,";
