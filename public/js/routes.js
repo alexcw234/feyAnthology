0cf7d56ef6b054d814406c34648ec39d0aea5050
Alex Watanabe <alexcw@hawaii.edu>
2016-03-25 10:49:07 -1000

    Clear routes.js

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,18 +1,1 @@
 var app = angular.module("browseApp", []);
-
-app.controller("myCtrl", function($scope) {
-    $scope.firstName = "If this is showing,";
-    $scope.lastName = "angular is working.";
-});
-
-
-app.controller("tableCtrl", function($scope, $http) {
-    $http.get("reqs/cats/showall")
-      .success(function(response)
-        {
-            console.log("Test");
-            $scope.cats = response;
-
-        });
-
-});
