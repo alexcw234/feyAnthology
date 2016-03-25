9d1dced63283f7a790d942822a65691bfd60f344
Alex Watanabe <alexcw@hawaii.edu>
2016-03-24 17:30:50 -1000

    Make more cat seeds and reseed

--- database/seeds/CatsSeeder.php
+++ database/seeds/CatsSeeder.php
@@ -13,8 +13,63 @@ class CatsSeeder extends Seeder
     {
 
         DB::table('categories')->insert([
-          'catName' => 'Sample',
+          'catName' => 'Sample1',
           'description' => 'A sample description',
+          'options' => json_encode(array
+          (
+            'public' => 1,
+            'allow_new_contributors' => 1,
+            )
+          ),
+        ]);
+
+        DB::table('categories')->insert([
+          'catName' => 'Sample2',
+          'description' => 'A 2nd sample description',
+          'options' => json_encode(array
+          (
+            'public' => 1,
+            'allow_new_contributors' => 1,
+            )
+          ),
+        ]);
+
+        DB::table('categories')->insert([
+          'catName' => 'Sample3',
+          'description' => 'A 3rd sample description',
+          'options' => json_encode(array
+          (
+            'public' => 1,
+            'allow_new_contributors' => 1,
+            )
+          ),
+        ]);
+
+        DB::table('categories')->insert([
+          'catName' => 'Sample4',
+          'description' => 'A 4th sample description',
+          'options' => json_encode(array
+          (
+            'public' => 1,
+            'allow_new_contributors' => 1,
+            )
+          ),
+        ]);
+
+        DB::table('categories')->insert([
+          'catName' => 'Sample5',
+          'description' => 'A 5th sample description',
+          'options' => json_encode(array
+          (
+            'public' => 1,
+            'allow_new_contributors' => 1,
+            )
+          ),
+        ]);
+
+        DB::table('categories')->insert([
+          'catName' => 'Sample6',
+          'description' => 'A 6th sample description',
           'options' => json_encode(array
           (
             'public' => 1,
