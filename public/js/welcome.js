7eb41a723ad19ed130b86b619d80b95032c17230
Alex Watanabe <alexcw@hawaii.edu>
2016-03-24 15:20:42 -1000

    A test of http.get

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -4,3 +4,12 @@ app.controller("myCtrl", function($scope) {
     $scope.firstName = "If this is showing,";
     $scope.lastName = "angular is working.";
 });
+
+
+app.controller("tableCtrl", function($scope, $http) {
+    $http.get("reqs/test.json")
+      .success(function(response)
+        {
+          $scope.cats = response;
+        });
+});
