efab1140ab48821647a159908b8203811b89c4f3
Alex Watanabe <alexcw@hawaii.edu>
2016-03-25 11:40:07 -1000

    Use angular.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
 <!--Angular scripts-->
-  <script src="bower_components/angular/angular.min.js"></script>
-  <script src="bower_components/angular-ui-router/release/angular-ui-router.min.js"></script>
+  <script src="bower_components/angular/angular.js"></script>
+  <script src="bower_components/angular-ui-router/release/angular-ui-router.js"></script>
 
 
 <!--End angular scripts-->
