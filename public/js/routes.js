33eecb5c8f02138f53ccc18f8e6c892e6c4b67b8
Alex Watanabe <alexcw@hawaii.edu>
2016-03-25 15:30:23 -1000

    Begin routing setup

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,1 +1,17 @@
-var app = angular.module("browseApp", []);
+var app = angular.module("browseApp", ['ui.router']);
+
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/test');
+
+
+    $stateProvider
+
+    .state('test',{
+        url: 'test',
+        templateUrl: 'test'
+    });
+
+
+});
