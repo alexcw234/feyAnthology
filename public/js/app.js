dbdc682f19adf79a04bf1075ab5d597313f351ee
Alex Watanabe <alexcw@hawaii.edu>
2016-03-26 17:30:42 -1000

    Add more files for specific controllers

--- public/js/app.js
+++ public/js/app.js
@@ -3,5 +3,8 @@ var app = angular.module("browseApp",
 // browseApp dependancies
   [
     'browseApp.routes',
-    'browseApp.controllers'
+    'browseApp.controllers',
+    'browseApp.cats_controllers',
+    'browseApp.newsub_controllers',
+    'browseApp.workselist_controllers.js'
   ]);
