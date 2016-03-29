44baaedb1a4511489774ca399ceddec7465ad1ea
Alex Watanabe <alexcw@hawaii.edu>
2016-03-29 13:55:44 -1000

    Add sample function

--- public/js/newsub.js
+++ public/js/newsub.js
@@ -1,6 +1,23 @@
 var app = angular.module("browseApp.newsub", []);
 
 
+app.controller = ('permissions', function($scope) {
+
+  $http.get("reqs/check/level")
+    .success(function(response)
+      {
+          console.log("Test");
+          $scope.cats = response;
+
+      });
+
+
+
+
+});
+
+
+
 app.controller = ('newSubmission',function($scope){
 
 
