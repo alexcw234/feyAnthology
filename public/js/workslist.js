a52f2b7af68bb6f9b02b9fa21f6015d004cfc87a
Alex Watanabe <alexcw@hawaii.edu>
2016-03-31 20:30:46 -1000

    Add workslist get function

--- public/js/workslist.js
+++ public/js/workslist.js
@@ -1,1 +1,14 @@
 var app = angular.module("browseApp.workslist", []);
+
+
+
+app.controller("workslistCtrl", function($scope, $http) {
+    $http.get("reqs/list/" . $scope.catID)
+      .success(function(response)
+        {
+            console.log("Test");
+            $scope.cats = response;
+
+        });
+
+});
