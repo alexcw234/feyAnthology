ca7f01e0f6c8f98c3dcbdd509626524a206a6aa7
Alex Watanabe <alexcw@hawaii.edu>
2016-04-03 17:41:43 -1000

    Write query string concat function

--- public/js/search.js
+++ public/js/search.js
@@ -2,12 +2,38 @@ var app = angular.module("browseApp.search", ['ngTagsInput']);
 
 app.controller("searchbox", function($state, $scope) {
 
+  $scope.formdata = {};
 
 
-  $scope.submitSearch = function($scope, $http) {
 
-      $scope.formdata = {};
+  $scope.submitSearch = function() {
 
+      // Convert jsons to query strings
+
+        var infojson = $scope.formdata;
+        var tagarray = $scope.tags;
+
+        var result = '';
+
+        for (key in infojson) {
+
+          result += key + '=' + infojson[key] + '&';
+
+        }
+
+          result += 'tags' + '=';
+
+        for (i in tagarray) {
+
+          tagjson = tagarray[i];
+
+          for (key in tagjson) {
+
+            result += tagjson[key] + ',';
+          }
+        }
+
+        console.log(result);
   };
 
 });
