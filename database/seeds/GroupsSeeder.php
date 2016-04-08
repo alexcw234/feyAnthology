a39af1ee30d200e8cc8185a45ef9168100bdd3a1
Alex Watanabe <alexcw@hawaii.edu>
2016-04-07 19:09:15 -1000

    Add ids to group seeds

--- database/seeds/GroupsSeeder.php
+++ database/seeds/GroupsSeeder.php
@@ -13,34 +13,42 @@ class GroupsSeeder extends Seeder
     public function run()
     {
         DB::table('groups')->insert([
+          'groupID' => 1,
           'groupName' => 'superadmin',
         ]);
 
         DB::table('groups')->insert([
+          'groupID' => 2,
           'groupName' => 'catadmin',
         ]);
 
         DB::table('groups')->insert([
+          'groupID' => 3,
           'groupName' => 'moderator',
         ]);
 
         DB::table('groups')->insert([
+          'groupID' => 4,
           'groupName' => 'contributor+',
-        ]);      
+        ]);
 
         DB::table('groups')->insert([
+          'groupID' => 5,
           'groupName' => 'contributor',
         ]);
 
         DB::table('groups')->insert([
+          'groupID' => 6,
           'groupName' => 'none',
         ]);
 
         DB::table('groups')->insert([
+          'groupID' => 7,
           'groupName' => 'limited',
         ]);
 
         DB::table('groups')->insert([
+          'groupID' => 8,
           'groupName' => 'banned',
         ]);
 
