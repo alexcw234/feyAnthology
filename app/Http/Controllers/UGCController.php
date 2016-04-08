936afcee1dc45394a7502c8e09e99bdd8e70fcb6
Alex Watanabe <alexcw@hawaii.edu>
2016-04-07 18:33:49 -1000

    Fill out skeleton for UGC Controller

--- app/Http/Controllers/UGCController.php
+++ app/Http/Controllers/UGCController.php
@@ -2,11 +2,101 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+use App\Repositories\UGCRepository as UGCRepository;
 
 class UGCController extends Controller
 {
-    //
+
+    private $ugc;
+
+    /**
+    * Injects repository
+    *
+    */
+    public function __construct(UGCRepository $ugc, Request $request)
+    {
+          $this->ugc = $ugc;
+          $this->request = $request;
+    }
+
+    /**
+   * Will display selection of works.
+   *
+   * @return Response
+   */
+  public function index($catID)
+  {
+
+  }
+
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function create()
+  {
+      //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store()
+  {
+      //
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function show($id)
+  {
+
+  }
+
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function edit($id)
+  {
+      //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function update($id)
+  {
+      //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
+      //
+  }
+
 }
