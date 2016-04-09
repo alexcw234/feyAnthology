9a558375d3c893cbb0a4ba3b9931e0fab7f5bda2
Alex Watanabe <alexcw@hawaii.edu>
2016-04-08 15:40:12 -1000

    Create UserRepository

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,5 +28,5 @@ class UserController extends Controller
         }
 
 
-        
+
 }
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories;
+
+use App\UGC;
+use App\User;
+use App\Group;
+use App\Category;
+use DB;
+use Response;
+
+class UserRepository {
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+ ?>
