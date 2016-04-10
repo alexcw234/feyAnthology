4726945255724b301b45c4c8ec92881364ab910a
Alex Watanabe <alexcw@hawaii.edu>
2016-04-10 11:42:56 -1000

    Attempt at dynamic form (not working currently)

--- public/js/newsub.js
+++ public/js/newsub.js
@@ -36,14 +36,75 @@ app.controller('newFormCtrl',function($scope) {
 
 app.controller('newFormDisplay',function($scope) {
 
+        $scope.newform;
+
         $scope.display = function(type) {
 
-          console.log(type);
-          console.log(typeof(type));
+          var shortfield = '<input style="width: 200;" ';
+          var longfield = '<input style="width: 450;" ';
+          var medfield = '<input style="width: 300;" ';
+          var boxfield = '<textarea style="width: 600; height: 100;" ';
 
-          var json = JSON.parse(type);
+  //        console.log(type);
+  //        console.log(typeof(type));
 
-          console.log(json);
+          var fields = JSON.parse(type);
+  //        console.log(fields);
+
+          for (field in fields)
+          {
+              var parameters = fields[field];
+
+              parameters = parameters.split(',');
+//              console.log(parameters);
+
+              var newfield = "";
+
+              // field size
+
+              if (parameters[0] == 'short')
+              {
+                newfield = newfield.concat(shortfield);
+
+              } else if (parameters[0] == 'long')
+              {
+                newfield = newfield.concat(longfield);
+
+              } else if (parameters[0] == 'medium')
+              {
+                newfield = newfield.concat(medfield);
+
+              } else if (parameters[0] == 'box')
+              {
+                newfield = newfield.concat(boxfield);
+              }
+
+              // append model name
+
+              newfield = newfield.concat(' ng-model="newSubmission.' + field + '"');
+
+              // if required
+              if (parameters[1] == 'true')
+              {
+                newfield = newfield.concat(' required>')
+              }
+              else
+              {
+                newfield = newfield.concat('>')
+              }
+
+              // close input tag if box
+
+              if (parameters[0] == 'box')
+              {
+                newfield = newfield.concat('</textarea>');
+              }
+
+              fields[field] = newfield;
+              console.log(fields[field]);
+          }
+
+          $scope.newform = fields;
 
         };
 
