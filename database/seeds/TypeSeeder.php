acac41b4323853aba689af7b8d7836b64ee48626
Alex Watanabe <alexcw@hawaii.edu>
2016-04-09 15:05:17 -1000

    Add proper types seeds and reseed

--- database/seeds/TypeSeeder.php
+++ database/seeds/TypeSeeder.php
@@ -7,18 +7,67 @@ class TypeSeeder extends Seeder
     /**
      * Run the database seeds.
      *
+     *  For expected fields, hstore specify as follows
+     *    "Field Name" => "length of field (Short, medium, long, box),
+     *                    required field (true,false), lock display on form at (top, bottom, none)"
+     *
      * @return void
      */
     public function run()
     {
         DB::table('types')->insert([
 
-          'contentType' => 'TEST',
+          'contentType' => 'Story',
           'expectedFields' =>  '
-            "TestField1" => "true",
-            "TestField2" => "true",
+            "Source" => "short,true,top",
+            "Title" => "short,true,none",
+            "Author" => "short,true,none",
+            "Summary" => "box,false,bottom",
           ',
         ]);
+
+        DB::table('types')->insert([
+
+          'contentType' => 'Art',
+          'expectedFields' =>  '
+            "Source" => "short,true,top",
+            "Title" => "short,false,none",
+            "Artist" => "short,true,none",
+            "Description" => "box,false,bottom",
+          ',
+        ]);
+
+        DB::table('types')->insert([
+
+          'contentType' => 'Blog',
+          'expectedFields' =>  '
+            "Blog Name" => "short,true,none",
+            "Owner" => "short,true,none",
+            "Description" => "box,false,bottom",
+          ',
+        ]);
+
+        DB::table('types')->insert([
+
+          'contentType' => 'Website',
+          'expectedFields' =>  '
+            "Site Name" => "short,true,none",
+            "Owner" => "short,false,none",
+            "Description" => "box,false,bottom",
+          ',
+        ]);
+
+        DB::table('types')->insert([
+
+          'contentType' => 'Video',
+          'expectedFields' =>  '
+            "Source" => "short,true,top",
+            "Title" => "short,true,none",
+            "By" => "short,true,none",
+            "Description" => "box,false,bottom",
+          ',
+        ]);
+
 
 
     }
