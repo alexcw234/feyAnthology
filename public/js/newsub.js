277da84ccf815a7142b72a9ce3cf003cc50ff975
Alex Watanabe <alexcw@hawaii.edu>
2016-04-10 08:33:34 -1000

    Remove interior span controller

--- public/js/newsub.js
+++ public/js/newsub.js
@@ -16,21 +16,23 @@ app.controller('permissions', function($scope, $http, $stateParams) {
 
 });
 
-app.controller('newFormdisplay',function($scope){
+app.controller('newFormCtrl',function($scope){
 
-
+    $scope.template = null;
 
 
 
 
 });
 
-app.controller('typeSelect',function($scope, $http){
+app.controller('newFormDisplay',function($scope, $http){
 
       $http.get("reqs/types/showall")
       .success(function(response){
 
         $scope.types = response;
+
+
       });
 
 });
