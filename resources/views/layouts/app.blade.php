a1a03665665c65cdfe9b8e23f47d870a6338ba45
Alex Watanabe <alexcw@hawaii.edu>
2016-04-16 17:08:09 -1000

    Add bootstrap script to html

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,7 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
+    <script src="bootstrap/ui-bootstrap-tpls-1.3.2.min.js"></script>
 
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
