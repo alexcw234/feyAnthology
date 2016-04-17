1aa46386e1743275c2ddad35b3d44ec631df5d1d
Alex Watanabe <alexcw@hawaii.edu>
2016-04-16 16:38:12 -1000

    Edit usercp layouts

--- public/js/usercp/mainctrls.js
+++ public/js/usercp/mainctrls.js
@@ -1,1 +1,22 @@
 var app = angular.module("userCP.mainctrls", []);
+
+
+
+app.controller("header", function($scope){
+
+});
+
+
+/*
+* Main controller for the usercp menu
+*
+*/
+app.controller("controller_m", function($scope, $state) {
+
+$scope.$parent.header = "User Control Panel";
+
+
+
+$scope.goToState = function(name){$state.go(name)};
+
+});
--- public/js/usercp/routes.js
+++ public/js/usercp/routes.js
@@ -1,1 +1,53 @@
-var app = angular.module("userCP.routes", []);
+var app = angular.module("userCP.routes", ['ui.router']);
+
+/*
+* Configurations for states for ui-router. Controllers refer to
+* the controllers located in mainctrls.js.
+*
+*/
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider
+
+    .otherwise('/menu');
+
+    $stateProvider
+
+    .state('menu',{
+        url: '/menu',
+        templateUrl: 'usercp/usermenu.html',
+        controller: 'controller_m',
+    })
+
+
+    .state('menu.mycats',{
+        url: '/mycats',
+        views: {
+              'mycats' : {
+              
+
+              }
+        }
+
+
+
+    });
+
+
+    .state('mod',{
+        url: '/mod',
+        templateUrl: 'usercp/pendinglist.html',
+        controller: 'controller_p',
+    })
+
+    .state('mod.one',{
+      url: '/:workID',
+      views: {
+            'entry' : {
+                templateUrl: 'usercp/pendinglist.entry.html',
+                controller: 'entry',
+              }
+        }
+    })
+
+});
