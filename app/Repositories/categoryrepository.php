b69ffa7e4a481afdc0dcfeb213e8f1c3abb6f4d9
Alex Watanabe <alexcw@hawaii.edu>
2016-04-18 23:02:22 -1000

    Attempt to fix global query

--- app/Repositories/categoryrepository.php
+++ app/Repositories/categoryrepository.php
@@ -15,10 +15,24 @@ class CategoryRepository {
 
   public function getuserCPTable($userID)
   {
-      return Category::join('usersgroupscats','categories.catID','=','usersgroupscats.catID')
-      ->join('groups','usersgroupscats.groupID','=','groups.groupID')
+/// Currently broken fix this 
+      return Category::join('usersgroupscats','categories.catID','=','usersgroupscats.catID', 'left outer')
+      ->join('groups','usersgroupscats.groupID','=','groups.groupID', 'left outer')
       ->select('categories.catID','catName','description','groups.groupName','level')
-      ->where('userID','=',$userID)->where('categories.catID','>',1)
+
+      ->where(function($query) use ($userID) {
+
+          if(!UGC::join('groups','usersgroupscats.groupID','=','groups.groupID')
+          ->select('groups.level')->where('catID','=',1)->where('userID','=',$userID)
+          ->where('groups.level','>',77)->get()->isEmpty())
+          {
+            $query->where('categories.catID','>',1);
+          }
+          else
+          {
+            $query->where('userID','=',$userID)->where('categories.catID','>',1);
+          }
+      })
       ->get();
 
   }
