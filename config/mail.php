12821d2db528ba04a83f46c545a6768471f8e759
Alex Watanabe <alexcw@hawaii.edu>
2016-04-26 19:06:30 -1000

    Specify email

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'feyanthology@gmail.com', 'name' => 'Fey Anthology'],
 
     /*
     |--------------------------------------------------------------------------
